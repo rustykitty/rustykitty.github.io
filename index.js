5fda5be7b8cf2785f8e37f43a40f161723169890
Rusty <rusty3925@gmail.com>
2025-06-14 09:26:23 -0700

    experiment (use jekyll?)

--- index.js
+++
@@ -1,7 +0,0 @@
-fetch('header.html')
-    .then(response => response.text())
-    .then(data => {
-        const header = document.querySelector('header');
-        if (header)
-            header.innerHTML = data;
-    });
