27c40c06f7661c1855895e159fc70f737026bb3d
Rusty <rusty3925@gmail.com>
2025-06-16 14:39:48 -0700

    changed URLs to open in new tab, added new image

---
+++ index.js
@@ -0,0 +1,9 @@
+// set external links to open in a new tab
+// https://stackoverflow.com/a/12042162
+$.expr[':'].external = function(obj) {
+    return !obj.href.match(/^mailto\:/)
+           && (obj.hostname !== location.hostname)
+           && !obj.href.match(/^javascript\:/)
+           && !obj.href.match(/^$/);
+};
+$('a:external').attr('target', '_blank');
