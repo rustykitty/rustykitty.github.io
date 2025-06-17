2513f2b98ca3a5b7d415b775ff7aeea4052e6c04
Rusty <rusty3925@gmail.com>
2025-06-17 08:42:07 -0700

    noreferrer too

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-// set external links to open in a new tab
-// https://stackoverflow.com/a/12042162
-$.expr[':'].external = function(obj) {
+$( document ).ready(function() {
+    // set external links to open in a new tab
+    // https://stackoverflow.com/a/12042162
+    $.expr[':'].external = function(obj) {
     return !obj.href.match(/^mailto\:/)
            && (obj.hostname !== location.hostname)
            && !obj.href.match(/^javascript\:/)
            && !obj.href.match(/^$/);
-};
-$('a:external').attr('target', '_blank');
+    };
+    $('a:external').attr('target', '_blank').attr('rel','noopener noreferrer');
+});
