6781777feb80cc2c6289ce392048c6a61b19dfbb
Rusty <rusty3925@gmail.com>
2025-08-25 07:22:25 -0700

    load header, footer, nav via js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+/**
+ * "Transcludes" the content of a page into an HTML element.
+ * This function does ZERO input validation, you have been warned.
+ * @param {*} url The URL to be loaded
+ * @param {*} element The element to load the content into
+ */
+async function includePage(url, element) {
+    if (typeof element === 'string') {
+        element = document.getElementById(element);
+    }
+    const response = await fetch(url);
+    element.innerHTML = await response.text();
+}
+
 $( document ).ready(function() {
     // set external links to open in a new tab
     // https://stackoverflow.com/a/12042162
@@ -9,3 +23,10 @@ $( document ).ready(function() {
     };
     $('a:external').addClass('external').attr('target', '_blank').attr('rel','noopener noreferrer');
 });
+
+$( document ).ready(function() {
+    includePage('include/header.html', document.querySelector('header'));
+    includePage('include/nav.html', document.querySelector('nav'));
+    includePage('include/footer.html', document.querySelector('footer'));
+})
+
